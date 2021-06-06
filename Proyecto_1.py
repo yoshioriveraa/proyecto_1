6816edfa5319dcb36911742c9c827ad7009ba7c5
yoshioriveraa <67650119+yoshioriveraa@users.noreply.github.com>
2021-06-06 00:28:12 -0500

    Update Proyecto_1.py

--- Proyecto_1.py
+++ Proyecto_1.py
@@ -4,28 +4,28 @@ start = str(input("Ingresa el operador para la practica: "))
 
 if start == "suma":
 	i=0
-	while i<5:
+	while i<5: #Se repetirá por 5 veces
 		i+=1
 		numeros ="1234567890"
 		digitos = 1
-		a="".join(random.sample(numeros,digitos))
+		a="".join(random.sample(numeros,digitos)) #Número aleatorio
 		b="".join(random.sample(numeros,digitos))
 		print(int(a), "+",int(b))
-		time.sleep(5)
+		time.sleep(5) # 5 segundos se demora
 		print(int(a) + int(b))
 		time.sleep(2)
 
-if start == "resta":
+if start == "multiplicacion":
 	i=0
-	while i<5:
+	while i<3: #Se repetirá por 3 veces
 		i+=1
 		numeros ="123456789"
 		digitos = 1
-		a="".join(random.sample(numeros,digitos))
+		a="".join(random.sample(numeros,digitos)) #Número aleatorio
 		b="".join(random.sample(numeros,digitos))
-		print(int(a), "-",int(b))
-		time.sleep(5)
-		print(int(a) - int(b))
+		print(int(a), "x",int(b))
+		time.sleep(8) # 8 segundos se demora
+		print(int(a) * int(b))
 		time.sleep(2)
 
 
